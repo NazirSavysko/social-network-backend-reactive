e590ac03213ba7e8359f959974ed836d9c7fa85e
Nazir Savysko <nazirsavisko@gmail.com>
2026-01-15 16:05:15 +0200

    initialize project structure with Maven and Spring Boot setup

---
+++ src/main/java/social/network/backend/reactive/SocialNetworkBackendReactiveApplication.java
@@ -0,0 +1,13 @@
+package social.network.backend.reactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialNetworkBackendReactiveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SocialNetworkBackendReactiveApplication.class, args);
+    }
+
+}
---
+++ src/test/java/social/network/backend/reactive/SocialNetworkBackendReactiveApplicationTests.java
@@ -0,0 +1,13 @@
+package social.network.backend.reactive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialNetworkBackendReactiveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
