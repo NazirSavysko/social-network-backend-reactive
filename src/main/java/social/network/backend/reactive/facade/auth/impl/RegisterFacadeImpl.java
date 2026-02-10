cd2be55eaebf77c9362c350c246077a9908ec66f
Nazir Savysko <nazirsavisko@gmail.com>
2026-02-10 18:19:35 +0200

    remove unused import for UserExistException in RegisterFacadeImpl

--- src/main/java/social/network/backend/reactive/facade/auth/impl/RegisterFacadeImpl.java
+++ src/main/java/social/network/backend/reactive/facade/auth/impl/RegisterFacadeImpl.java
@@ -1,18 +1,14 @@
 package social.network.backend.reactive.facade.auth.impl;
 
-import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
 import social.network.backend.reactive.dto.auth.RegisterDTO;
-import social.network.backend.reactive.exception.UserExistException;
 import social.network.backend.reactive.facade.auth.RegisterFacade;
 import social.network.backend.reactive.mapper.auth.GetRegistrationMapper;
 import social.network.backend.reactive.service.auth.RegistrationService;
 import social.network.backend.reactive.service.user.UserReadService;
 import social.network.backend.reactive.service.user.UserWriteService;
-
-import static java.lang.String.format;
 
 @Component
 @RequiredArgsConstructor
